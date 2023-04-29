5f6067d80c77795c04fa469fdc2a96118dbbff1c
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2023-04-29 16:26:31 +0530

    feat: Added new navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/header/Navbar';
+import Newnav from './components/newnavbar/Newnav';
 
 function App() {
   return (
     <>
     <Navbar/>
+    <Newnav/>
     </>
   );
 }
--- client/src/components/header/Navbar.js
+++ client/src/components/header/Navbar.js
@@ -6,35 +6,35 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import Avatar from '@mui/material/Avatar';
 
 const Navbar = () => {
-  return (
-    <header>
-      <nav>
-        <div className="left">
-          <div className="navlogo">
-            <img src="./amazon_PNG25.png" alt="" />
-          </div>
-          <div className="nav_searchbar ">
-            <input type="text" name="" id="" />
-            <div className="search_icon">
-              <SearchIcon id="search" />
-            </div>
-          </div>
-        </div>
-        <div className="right">
-          <div className="nav_btn">
-            <a href="/">signin</a>
-          </div>
-          <div className="cart_btn">
-          <Badge badgeContent={4} color="primary">
-       <ShoppingCartIcon id="icon"/>
-    </Badge>
-    <p>Cart</p>
-    <Avatar className='avtar'/>
-          </div>
-        </div>
-      </nav>
-    </header>
-  )
+    return (
+        <header>
+            <nav>
+                <div className="left">
+                    <div className="navlogo">
+                        <img src="./amazon_PNG25.png" alt="" />
+                    </div>
+                    <div className="nav_searchbar ">
+                        <input type="text" name="" id="" />
+                        <div className="search_icon">
+                            <SearchIcon id="search" />
+                        </div>
+                    </div>
+                </div>
+                <div className="right">
+                    <div className="nav_btn">
+                        <a href="/">signin</a>
+                    </div>
+                    <div className="cart_btn">
+                        <Badge badgeContent={4} color="primary">
+                            <ShoppingCartIcon id="icon" />
+                        </Badge>
+                        <p>Cart</p>
+                        <Avatar className='avtar' />
+                    </div>
+                </div>
+            </nav>
+        </header>
+    )
 }
 
 export default Navbar
