20671eaa99fcab2945d5f28d0eba3fc4994783ba
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2023-05-04 21:12:07 +0530

    feat: Added footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import Navbar from './components/header/Navbar';
 import Newnav from './components/newnavbar/Newnav';
-import Maincomp from './components/home/Maincomp'
+import Maincomp from './components/home/Maincomp';
+import Footer from './components/footer/Footer'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <Navbar/>
     <Newnav/>
     <Maincomp/>
+    <Footer/>
     </>
   );
 }
